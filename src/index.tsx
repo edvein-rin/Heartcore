231cbe180f63e5159a39e04d2cb73f4c62ca3bb0
edvein-rin <edveinrin@gmail.com>
2021-07-15 15:16:10 +0300

    Globals

--- src/index.tsx
+++ src/index.tsx
@@ -1,44 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createGlobalStyle } from "styled-components/macro";
 
-// import 'normalize.css'
+import 'normalize.css'
 
+import GlobalColors from './components/globals/Colors'
+import GlobalStyles from './components/globals/Styles'
 import App from './components/organisms/App'
 
-const GlobalStyles = createGlobalStyle`
-  @import url('http://fonts.cdnfonts.com/css/gilroy-bold');
-  @import url('http://fonts.cdnfonts.com/css/recoleta');
-
-
-  html,
-  body,
-  div#root {
-    padding: 0;
-    margin: 0;
-    width: 100%;
-    height: 100%;
-  }
-
-  body {
-    font-family: 'Gilroy-Regular', sans-serif;
-    color: #2b2b2b;
-  }
-
-  h1 {
-    margin-top: 0px;
-    margin-bottom: 30px;
-
-    font-family: 'Recoleta', sans-serif;
-    font-size: 85px;
-    font-weight: 400;
-    line-height: 85px;
-    letter-spacing: 0.01em;
-  }
-`
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalColors />
     <GlobalStyles />
     <App />
   </React.StrictMode>,
