224d4dc1f1eb495a1229b17720b6f90111c66fbd
edvein-rin <edveinrin@gmail.com>
2021-07-16 13:55:51 +0300

    Home Page

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,1 +1,17 @@
-export {}
+import styled from 'styled-components/macro'
+
+function Home () {
+  return (
+    <Wrapper>
+      Home Page
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export default Home
