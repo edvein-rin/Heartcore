0c26c4c216a29c5b20dde64feb89178010e620da
edvein-rin <edveinrin@gmail.com>
2021-07-16 13:55:39 +0300

    About page

---
+++ src/components/pages/About.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components/macro'
+
+function About () {
+  return (
+    <Wrapper>
+      About Page
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export default About
