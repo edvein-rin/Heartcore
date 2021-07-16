9d74ea1c1cc5989b4d1aa780e2ad633fe0c1d63e
edvein-rin <edveinrin@gmail.com>
2021-07-16 15:35:02 +0300

    Section

---
+++ src/components/atoms/Section.tsx
@@ -0,0 +1,25 @@
+import { PropsWithChildren } from 'react'
+import styled from 'styled-components/macro'
+
+
+export interface SectionProps extends PropsWithChildren<{}> {
+  className?: string,
+}
+
+const Section = ({children, className}: SectionProps) => {
+  return (
+    <Wrapper className={className}>
+      {children}
+    </Wrapper>
+  )
+}
+
+export const Wrapper = styled.div<SectionProps>`
+  display: flex;
+
+  width: 100%;
+
+  box-sizing: border-box;
+`
+
+export default Section
