bad2b83d0cbd8f7f7d967c130b9e659f6386ac48
edvein-rin <edveinrin@gmail.com>
2021-07-16 13:56:10 +0300

    Values Page

--- src/components/pages/Values.tsx
+++ src/components/pages/Values.tsx
@@ -1,1 +1,17 @@
-export {}
+import styled from 'styled-components/macro'
+
+function Values () {
+  return (
+    <Wrapper>
+      Values Page
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export default Values
