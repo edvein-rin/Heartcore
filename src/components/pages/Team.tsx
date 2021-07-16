4471df7723d99b8f9b1f8f085d3148f78e670893
edvein-rin <edveinrin@gmail.com>
2021-07-16 13:56:01 +0300

    Team Page

--- src/components/pages/Team.tsx
+++ src/components/pages/Team.tsx
@@ -1,1 +1,17 @@
-export {}
+import styled from 'styled-components/macro'
+
+function Team () {
+  return (
+    <Wrapper>
+      Team Page
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export default Team
