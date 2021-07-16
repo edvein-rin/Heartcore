23fe74cfbc3ff7abea36e2fa2d212afaa75883ef
edvein-rin <edveinrin@gmail.com>
2021-07-16 13:54:27 +0300

    Colors

--- src/components/globals/Colors.tsx
+++ src/components/globals/Colors.tsx
@@ -3,6 +3,21 @@ import { createGlobalStyle } from 'styled-components/macro'
 
 export default createGlobalStyle`
   html {
-    --background-color: #000;
+    --background-color: #fff;
+    --color: #2b2b2b;
+    --highlight-color: #f88e53;
+
+    --gray-container-bg-color: #edf0f7;
+    --gray-container-color: #4d4d4d;
+    --blue-container-bg-color: #293b78;
+    --blue-container-color: #fff;
+    --orange-container-bg-color: #fdba84;
+    --orange-container-color: #893c01;
+    --yellow-container-bg-color: #f9e39d;
+    --yellow-container-color: #893c01;
+    --light-blue-container-bg-color: #8cb8e1;
+    --light-blue-container-color: #0b5194;
+    --green-container-bg-color: #a8d7d1;
+    --green-container-color: #1b5f57;
   }
 `
