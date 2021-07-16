b476dd6767f4c22996c580722abbc57834da6f96
edvein-rin <edveinrin@gmail.com>
2021-07-16 13:55:28 +0300

    Footer

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,1 +1,23 @@
-export {}
+import styled from 'styled-components/macro'
+import { Link } from 'react-router-dom'
+
+
+function Footer () {
+  return (
+    <Wrapper>
+      <Link to='/'>Home</Link>
+      <Link to='/about'>About</Link>
+      <Link to='/thesis'>Thesis</Link>
+      <Link to='/companies'>Companies</Link>
+      <Link to='/team'>Team</Link>
+      <Link to='/values'>Values</Link>
+      <Link to='/insights'>Insights</Link>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.footer`
+  background-color: var(--gray-container-bg-color);
+`
+
+export default Footer
