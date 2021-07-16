087a35e0027570994393d165f624decd882c45f0
edvein-rin <edveinrin@gmail.com>
2021-07-16 13:54:56 +0300

    Router

--- src/components/organisms/App.tsx
+++ src/components/organisms/App.tsx
@@ -1,21 +1,48 @@
+import { Route, Switch } from 'react-router-dom'
 import styled from 'styled-components/macro'
+
+import Home from '../pages/Home'
+import About from '../pages/About'
+import Thesis from '../pages/Thesis'
+import Companies from'../pages/Companies'
+import Team from '../pages/Team'
+import Values from '../pages/Values'
+import Insights from '../pages/Insights'
+
+import Footer from './Footer'
+import Header from './Header'
 
 
 function App() {
   return (
     <Wrapper>
-      <h1>Investing in happiness.</h1>
+      <Header />
+      <main>
+        <Switch>
+          <Route path='/' exact><Home /></Route>
+          <Route path='/about'><About /></Route>
+          <Route path='/thesis'><Thesis /></Route>
+          <Route path='/companies'><Companies /></Route>
+          <Route path='/team'><Team /></Route>
+          <Route path='/values'><Values /></Route>
+          <Route path='/insights'><Insights /></Route>
+        </Switch>
+      </main>
+      <Footer />
     </Wrapper>
   )
 }
 
 const Wrapper = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
+  flex-direction: column;
 
   width: 100%;
   height: 100%;
+
+  & > * {
+    width: 100%;
+  }
 `
 
 export default App
