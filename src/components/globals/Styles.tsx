92de6359b40342cf68f37ce9e6cf7a0f458ab454
edvein-rin <edveinrin@gmail.com>
2021-07-16 13:54:45 +0300

    Basic styling

--- src/components/globals/Styles.tsx
+++ src/components/globals/Styles.tsx
@@ -7,23 +7,13 @@ export default createGlobalStyle`
   div#root {
     padding: 0;
     margin: 0;
+
     width: 100%;
     height: 100%;
-  }
 
-  body {
+    background-color: var(--background-color);
+    color: var(--color);
+
     font-family: 'Gilroy-Regular', sans-serif;
-    color: #2b2b2b;
-  }
-
-  h1 {
-    margin-top: 0px;
-    margin-bottom: 30px;
-
-    font-family: 'Recoleta', sans-serif;
-    font-size: 85px;
-    font-weight: 400;
-    line-height: 85px;
-    letter-spacing: 0.01em;
   }
 `
