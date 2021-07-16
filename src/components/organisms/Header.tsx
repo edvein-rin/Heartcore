114d794083768e752c7f40275f69340b2b0a823f
edvein-rin <edveinrin@gmail.com>
2021-07-16 13:55:32 +0300

    Header

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,2 +1,21 @@
-export {}
+import styled from 'styled-components/macro'
+import { Link } from 'react-router-dom'
 
+
+function Header () {
+  return (
+    <Wrapper>
+      <Link to='/'>Home</Link>
+      <Link to='/about'>About</Link>
+      <Link to='/thesis'>Thesis</Link>
+      <Link to='/companies'>Companies</Link>
+      <Link to='/team'>Team</Link>
+      <Link to='/values'>Values</Link>
+      <Link to='/insights'>Insights</Link>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.header``
+
+export default Header
