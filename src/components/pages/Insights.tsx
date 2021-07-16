da04257a14631f2bd641cd3e02ab66a278893fd6
edvein-rin <edveinrin@gmail.com>
2021-07-16 13:55:57 +0300

    Insights Page

--- src/components/pages/Insights.tsx
+++ src/components/pages/Insights.tsx
@@ -1,1 +1,17 @@
-export {}
+import styled from 'styled-components/macro'
+
+function Insights () {
+  return (
+    <Wrapper>
+      Insights Page
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export default Insights
