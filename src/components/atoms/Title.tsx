ba8ac2407e72e8ba4bc75dcf2eac8f76c7686a5e
edvein-rin <edveinrin@gmail.com>
2021-07-19 16:48:46 +0300

    Title

---
+++ src/components/atoms/Title.tsx
@@ -0,0 +1,42 @@
+import { PropsWithChildren } from 'react';
+import styled from 'styled-components/macro'
+import React from 'react'
+
+import { Wrapper as HeadingWrapper } from '../molecules/Heading'
+
+
+export interface TitleProps extends PropsWithChildren<{}> {
+  align?: 'left' | 'right' | 'center'
+}
+
+export default function Title ({children, align = 'left'}: TitleProps) {
+  return (
+    <Wrapper align={align}>
+      <h1>{children}</h1>
+    </Wrapper>
+  )
+}
+
+export const Wrapper = styled.div<TitleProps>`
+  display: flex;
+  justify-content: ${({align}) => {
+    switch (align) {
+      case 'left':
+        return 'flex-start'
+      case 'right':
+        return 'flex-end'
+      case 'center':
+        return 'center'
+    }
+  }};
+
+  ${HeadingWrapper} > & > h1 {
+    margin-top: 0px;
+    margin-bottom: 30px;
+    font-family: Recoleta, sans-serif;
+    font-size: 85px;
+    line-height: 85px;
+    font-weight: 400;
+    letter-spacing: 0.01em;
+  }
+`
