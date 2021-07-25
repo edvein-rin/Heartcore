17603ad9e7800fb45340935f38c1ec8a2c92b27e
edvein-rin <edveinrin@gmail.com>
2021-07-25 17:38:20 +0300

    Button ench

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -3,21 +3,23 @@ import styled, { css } from 'styled-components/macro'
 
 
 export interface Props extends PropsWithChildren<{}> {
-  variant?: 'text',
+  display?: 'block' | 'inline' | 'flex',
+  variant?: 'text' | 'no-style',
   textCase?: 'lowercase' | 'uppercase' | 'capitalize' | 'none',
   bold?: boolean,
   italic?: boolean,
 }
 
-export default function Button ({children, variant = 'text', textCase = 'uppercase', bold = true, italic = false}: Props) {
+export default function Button ({children, display = 'block', variant = 'text', textCase = 'uppercase', bold = true, italic = false}: Props) {
   return (
-    <Wrapper variant={variant} textCase={textCase} bold={bold} italic={italic}>
+    <Wrapper display={display} variant={variant} textCase={textCase} bold={bold} italic={italic}>
       {children}
     </Wrapper>
   )
 }
 
 export const Wrapper = styled.div<Props>`
+  display: ${({display}) => display};
   font-weight: ${({bold}) => bold ? 'bold' : 'inherit'};
   font-style: ${({italic}) => italic ? 'italic' : 'inherit'};
   
