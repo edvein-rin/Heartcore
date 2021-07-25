2b71486f8865479695e369c522a92356b03e5a2f
edvein-rin <edveinrin@gmail.com>
2021-07-25 16:31:59 +0300

    Subtitle rework

--- src/components/atoms/Subtitle.tsx
+++ src/components/atoms/Subtitle.tsx
@@ -1,38 +1,16 @@
-import { PropsWithChildren } from 'react';
-import styled from 'styled-components/macro'
+import { PropsWithChildren } from 'react'
 
-import { Wrapper as HeadingWrapper } from '../molecules/Heading'
+import Typography, { Align } from './Typography'
 
 
 export interface Props extends PropsWithChildren<{}> {
-  align?: 'left' | 'right' | 'center'
+  align?: Align,
 }
-
 
 export default function Subtitle ({children, align='left'}: Props) {
   return (
-    <Wrapper align={align}>{children}</Wrapper>
+    <Typography variant='h5' align={align}>
+      {children}
+    </Typography>
   )
 }
-
-export const Wrapper = styled.div<Props>`
-  display: flex;
-  justify-content: ${({align}) => {
-    switch (align) {
-      case 'left':
-        return 'flex-start'
-      case 'right':
-        return 'flex-end'
-      case 'center':
-        return 'center'
-    }
-  }};
-
-  ${HeadingWrapper} > & {
-    opacity: 0.8;
-    font-size: 24px;
-    margin-bottom: 0px;
-    margin-top: 0px;
-    line-height: 130%;
-  }
-`
