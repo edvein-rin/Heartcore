d5a67f9604f04921c56fdaed2980902970d19d31
edvein-rin <edveinrin@gmail.com>
2021-07-25 16:32:06 +0300

    Title rework

--- src/components/atoms/Title.tsx
+++ src/components/atoms/Title.tsx
@@ -1,42 +1,16 @@
 import { PropsWithChildren } from 'react';
-import styled from 'styled-components/macro'
 
-import { Wrapper as HeadingWrapper } from '../molecules/Heading'
+import Typography, { Align } from './Typography';
 
 
 export interface Props extends PropsWithChildren<{}> {
-  align?: 'left' | 'right' | 'center'
+  align?: Align,
 }
-
 
 export default function Title ({children, align = 'left'}: Props) {
   return (
-    <Wrapper align={align}>
-      <h1>{children}</h1>
-    </Wrapper>
+    <Typography variant='h1' align={align} paragraph>
+      {children}
+    </Typography>
   )
 }
-
-export const Wrapper = styled.div<Props>`
-  display: flex;
-  justify-content: ${({align}) => {
-    switch (align) {
-      case 'left':
-        return 'flex-start'
-      case 'right':
-        return 'flex-end'
-      case 'center':
-        return 'center'
-    }
-  }};
-
-  ${HeadingWrapper} > & > h1 {
-    margin-top: 0px;
-    margin-bottom: 30px;
-    font-family: Recoleta, sans-serif;
-    font-size: 85px;
-    line-height: 85px;
-    font-weight: 400;
-    letter-spacing: 0.01em;
-  }
-`
