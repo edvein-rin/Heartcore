d7c56e21badd489a81fe04b5610421628c654cb0
edvein-rin <edveinrin@gmail.com>
2021-07-28 15:51:59 +0300

    Adaptive fonts

--- src/components/atoms/Typography.tsx
+++ src/components/atoms/Typography.tsx
@@ -1,6 +1,9 @@
 import React, { PropsWithChildren } from 'react';
 import styled, { css } from 'styled-components/macro'
+
 import { Color } from '../globals/Colors';
+import breakpoints from '../globals/Breakpoints'
+
 
 export type Align = 'left' | 'center' | 'right'
 export type Variant = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'subtitle1' | 'subtitle2' | 'body1' | 'body2' | 'caption' | 'button' | 'overline'
@@ -50,42 +53,109 @@ export const Wrapper = styled.span<Props>`
       case 'h1':
         return css`
           font-family: Recoleta, sans-serif;
-          font-size: 85px;
-          line-height: 85px;
-          font-weight: 400;
-          letter-spacing: 0.01em;
+          letter-spacing: ${1.5/16}rem;
+          font-weight: 300;
+
+          @media ${breakpoints.down('mobileL')} { font-size: 3.5rem; }
+          @media ${breakpoints.up('mobileL')} { font-size: 4.7rem; }
+          @media ${breakpoints.up('tablet')} { font-size: 5.3rem; }
+          @media ${breakpoints.up('laptop')} { font-size: 5.9rem; }
+        `
+
+      case 'h2':
+        return css`
+          letter-spacing: ${-0.5/16}rem;
+          font-weight: 300;
+
+          @media ${breakpoints.down('mobileL')} { font-size: 2.375rem; }
+          @media ${breakpoints.up('mobileL')} { font-size: 2.9167rem; }
+          @media ${breakpoints.up('tablet')} { font-size: 3.3333rem; }
+          @media ${breakpoints.up('laptop')} { font-size: 3.75rem; }
+        `
+
+      case 'h3':
+        return css`
+          letter-spacing: 0;
+
+          @media ${breakpoints.down('mobileL')} { font-size: 2.375rem; }
+          @media ${breakpoints.up('mobileL')} { font-size: 2.9167rem; }
+          @media ${breakpoints.up('tablet')} { font-size: 3.3333rem; }
+          @media ${breakpoints.up('laptop')} { font-size: 3.75rem; }
         `
 
       case 'h4':
         return css`
           font-family: Recoleta, sans-serif;
-          font-size: 26px;
-          line-height: 120%;
-          letter-spacing: -0.02em;
-          font-weight: 400;
+          letter-spacing: ${0.25/16}rem;
+          font-weight: 500;
+
+          @media ${breakpoints.down('mobileL')} { font-size: 1.5625rem; }
+          @media ${breakpoints.up('mobileL')} { font-size: 1.8219rem; }
+          @media ${breakpoints.up('tablet')} { font-size: 2.0243rem; }
         `
 
       case 'h5':
         return css`
-          opacity: 0.8;
-          font-size: 24px;
-          margin-bottom: 0px;
-          margin-top: 0px;
-          font-weight: 400;
-          line-height: 130%;
+          letter-spacing: 0;
+          font-weight: 500;
+
+          @media ${breakpoints.down('mobileL')} { font-size: 1.25rem; }
+          @media ${breakpoints.up('mobileL')} { font-size: 1.3118rem; }
+          @media ${breakpoints.up('tablet')} { font-size: 1.4993rem; }
+        `
+
+      case 'h6':
+        return css`
+          letter-spacing: ${0.15/16}rem;
+          font-weight: 500;
+          
+          @media ${breakpoints.down('mobileL')} { font-size: 1.125rem; }
+          @media ${breakpoints.up('mobileL')} { font-size: 1.25rem; }
+        `
+
+      case 'subtitle1':
+        return css`
+          font-size: 1rem;
+          letter-spacing: ${0.15/16}rem;
+        `
+
+      case 'subtitle2':
+        return css`
+          font-size: 0.875rem;
+          letter-spacing: ${0.1/16}rem;
+          font-weight: 500;
+        `
+
+      case 'body1':
+        return css`
+          font-size: 1rem;
+          letter-spacing: ${0.5/16}rem;
         `
       
       case 'body2':
         return css`
-          font-size: 14px;
+          font-size: 0.875rem;
+          letter-spacing: ${0.25/16}rem;
+        `
+
+      case 'button':
+        return css`
+          font-size: 0.875rem;
+          letter-spacing: ${1.25/16}rem;
+          font-weight: 600;
         `
 
       case 'caption':
         return css`
-          font-size: 12px;
-          line-height: 14px;
-          letter-spacing: 0.09em;
-          weight: 600;
+          font-size: 0.75rem;
+          letter-spacing: ${0.4/16}rem;
+          font-weight: 600;
+        `
+
+      case 'overline':
+        return css`
+          font-size: 0.625rem;
+          letter-spacing: ${1.5/16}rem;
         `
     }
   }}
