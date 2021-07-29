93974d12eb1195b1df334ae18efbd7d12f5f14ea
edvein-rin <edveinrin@gmail.com>
2021-07-29 16:16:17 +0300

    Menu btn alignment

--- src/components/atoms/MenuButton.tsx
+++ src/components/atoms/MenuButton.tsx
@@ -1,3 +1,5 @@
+import styled from 'styled-components'
+
 import Button, { Props as ButtonProps } from './Button'
 import Typography from './Typography'
 import Burger from './Burger'
@@ -8,10 +10,18 @@ export type Props = ButtonProps
 
 export default function MenuButton (props: Props) {
   return (
-    <Button {...props} display='flex'>
-      <Burger />
-      <Spacer size={10} axis='horizontal'></Spacer>
-      <Typography variant='caption'>Menu</Typography>
+    <Button {...props} variant='no-style'>
+      <Wrapper>
+        <Burger />
+        <Spacer size={10} axis='horizontal'></Spacer>
+        <Typography variant='button'>Menu</Typography>
+      </Wrapper>
     </Button>
   )
 }
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
