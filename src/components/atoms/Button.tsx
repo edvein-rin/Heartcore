60c2c6801ff7afcba616555d57546ac6dc72d82b
edvein-rin <edveinrin@gmail.com>
2021-07-29 16:15:57 +0300

    Conditional render for text btn

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,5 +1,6 @@
 import { PropsWithChildren } from 'react'
 import styled, { css } from 'styled-components/macro'
+import Typography from './Typography'
 
 
 export interface Props extends PropsWithChildren<{}> {
@@ -13,7 +14,7 @@ export interface Props extends PropsWithChildren<{}> {
 export default function Button ({children, display = 'block', variant = 'text', textCase = 'uppercase', bold = true, italic = false}: Props) {
   return (
     <Wrapper display={display} variant={variant} textCase={textCase} bold={bold} italic={italic}>
-      {children}
+      {variant === 'text' ? <Typography variant='button'>{children}</Typography> : children}
     </Wrapper>
   )
 }
@@ -22,6 +23,8 @@ export const Wrapper = styled.div<Props>`
   display: ${({display}) => display};
   font-weight: ${({bold}) => bold ? 'bold' : 'inherit'};
   font-style: ${({italic}) => italic ? 'italic' : 'inherit'};
+
+  cursor: pointer;
   
   ${({variant, textCase}) => {
     switch (variant) {
