22f10a32db1234f4d5e80fcfbed1b15f38c1beb4
edvein-rin <edveinrin@gmail.com>
2021-07-29 16:17:01 +0300

    Font face for fonts

--- src/components/globals/Styles.tsx
+++ src/components/globals/Styles.tsx
@@ -2,6 +2,24 @@ import { createGlobalStyle } from 'styled-components/macro'
 
 
 export default createGlobalStyle`
+  @font-face {
+    font-family: Gilroy;
+    src: url('https://uploads-ssl.webflow.com/6023c6394b9a4d541b90dcf4/6023c6394b9a4d9d3f90dd1d_Gilroy-Regular.otf');
+    font-weight: normal;
+  }
+
+  @font-face {
+    font-family: Gilroy;
+    src: url('https://uploads-ssl.webflow.com/6023c6394b9a4d541b90dcf4/6023c6394b9a4dd77390dd1e_Gilroy-Semibold.otf');
+    font-weight: bold;
+  }
+
+  @font-face {
+    font-family: Recoleta;
+    src: url('https://uploads-ssl.webflow.com/6023c6394b9a4d541b90dcf4/6023c6394b9a4d618f90dd1c_Recoleta%20Regular.otf');
+  }
+
+
   html,
   body,
   div#root {
@@ -14,7 +32,7 @@ export default createGlobalStyle`
     background-color: var(--background-color);
     color: var(--color);
 
-    font-family: 'Gilroy-Regular', sans-serif;
+    font-family: Gilroy, sans-serif;
   }
 
   * {
