34a482aec879febd7d2afd5094cbb1cb54d8a34f
edvein-rin <edveinrin@gmail.com>
2021-07-29 16:17:39 +0300

    Test cards

--- src/components/templates/Home.tsx
+++ src/components/templates/Home.tsx
@@ -1,8 +1,11 @@
 import styled from 'styled-components/macro'
 
+import aboutCardImage from '../../assets/cards/about.png'
+
 import Container from '../atoms/Container'
 import Section from '../atoms/Section'
 import Heading from '../molecules/Heading'
+import Card from '../molecules/Card'
 
 
 export interface Props {
@@ -25,7 +28,60 @@ export default function Home (props: Props) {
       </HeaderSection>
       <CardsSection>
         <CardsContainer>
-          {/* <Card title='' subtitle=''></Card> */}
+          <Card
+            overline='about'
+            title='100% consumer tech'
+            description='Specialization drives success'
+            image={aboutCardImage}
+            bgColor='blue'
+            textColor='white'
+            noise={true}
+          />
+          <Card
+            overline='thesis'
+            title='Making people happy'
+            description='We invest in companies re-enchanting the world through technology'
+            image={aboutCardImage}
+            bgColor='light-grey'
+            textColor='grey'
+            noise={true}
+          />
+          <Card
+            overline='companies'
+            title={<span>Our <em>raison d'être</em></span>}
+            description="We're proud to have been among the first investors in 85 category-defining consumer technology companies"
+            image={aboutCardImage}
+            bgColor='light-orange'
+            textColor='dark-orange'
+            noise={true}
+          />
+          <Card
+            overline='team'
+            title='Global at heart'
+            description='A group of investors in love with our craft and passionate about entrepreneurship'
+            image={aboutCardImage}
+            bgColor='light-yellow'
+            textColor='dark-orange'
+            noise={true}
+          />
+          <Card
+            overline='values'
+            title='Truth + Love = Growth'
+            description='Radically different venture capital'
+            image={aboutCardImage}
+            bgColor='light-blue'
+            textColor='blue'
+            noise={true}
+          />
+          <Card
+            overline='insights'
+            title='Sharing is caring'
+            description='Everything you’ve always wanted to know about consumer technology'
+            image={aboutCardImage}
+            bgColor='lighter-blue'
+            textColor='dark-green'
+            noise={true}
+          />
         </CardsContainer> 
       </CardsSection>
     </Wrapper>
@@ -39,12 +95,12 @@ export const Wrapper = styled.div`
 `
 
 export const HeaderSection = styled(Section)`
-  padding-top: 158px;
-  padding-bottom: 125px;
+  padding-top: 80px;
+  padding-bottom: 80px;
 `
 
 export const CardsSection = styled(Section)`
-  
+  padding-bottom: 80px;
 `
 
 export const CardsContainer = styled(Container)`
