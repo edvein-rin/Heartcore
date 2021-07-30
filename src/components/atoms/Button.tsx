7c043130b82eb9c3794110f80563c770cd2ba044
edvein-rin <edveinrin@gmail.com>
2021-07-30 15:48:14 +0300

    Button ench

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -6,43 +6,49 @@ import Typography from './Typography'
 export interface Props {
   display?: 'block' | 'inline' | 'flex',
   variant?: 'text' | 'no-style',
+  size?: 'normal' | 'big',
   textCase?: 'lowercase' | 'uppercase' | 'capitalize' | 'none',
   textColor?: Color,
   textHoverColor?: Color,
   bold?: boolean,
   italic?: boolean,
+  onClick?: React.MouseEventHandler<HTMLButtonElement>,
   children?: React.ReactNode,
 }
 
 export default function Button ({
-                              children,
-                              display,
-                              variant = 'text',
-                              textCase = 'uppercase',
-                              textColor,
-                              textHoverColor = 'orange',
-                              bold = true,
-                              italic = false,
+                                  children,
+                                  display,
+                                  variant = 'text',
+                                  size,
+                                  textCase = 'uppercase',
+                                  textColor,
+                                  textHoverColor = 'orange',
+                                  bold = true,
+                                  italic,
+                                  onClick
                                 }: Props) {
   return (
-    <Wrapper display={display} variant={variant} textCase={textCase} textColor={textColor} textHoverColor={textHoverColor} bold={bold} italic={italic}>
-      {variant === 'text' ? <Typography variant='button'>{children}</Typography> : children}
+    <Wrapper display={display} variant={variant} size={size} textCase={textCase} textColor={textColor} textHoverColor={textHoverColor} bold={bold} italic={italic} onClick={onClick}>
+      {variant === 'text' ?
+        (
+          size === 'big' ?
+            <Typography variant='h4' textCase='uppercase' weight={600} color={textColor} hoverColor={textHoverColor}>{children}</Typography>
+          : <Typography variant='button' color={textColor} hoverColor={textHoverColor}>{children}</Typography>
+        ) : children}
     </Wrapper>
   )
 }
 
-export const Wrapper = styled.div<Props>`
+export const Wrapper = styled.button<Props>`
   display: ${({display}) => display};
   
-  font-weight: ${({bold}) => bold ? 'bold' : 'inherit'};
-  font-style: ${({italic}) => italic ? 'italic' : 'inherit'};
-  color: ${({textColor}) => 'var(--' + textColor + ')'};
+  font-weight: ${({bold}) => bold && (bold ? 'bold' : 'inherit')};
+  font-style: ${({italic}) => italic && (italic ? 'italic' : 'inherit')};
 
   cursor: pointer;
-
-  &:hover {
-    color: ${({textHoverColor}) => 'var(--' + textHoverColor + ')'};
-  }
+  background: none;
+  border: none;
   
   ${({variant, textCase}) => {
     switch (variant) {
