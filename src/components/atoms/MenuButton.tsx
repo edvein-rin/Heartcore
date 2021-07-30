39255ac275a3d04ea5e402969e8237bf4eec3f5a
edvein-rin <edveinrin@gmail.com>
2021-07-30 15:48:50 +0300

    Menu button finished

--- src/components/atoms/MenuButton.tsx
+++ src/components/atoms/MenuButton.tsx
@@ -1,22 +1,41 @@
 import styled from 'styled-components'
+import { useEffect, useState } from 'react'
 
 import Button, { Props as ButtonProps } from './Button'
 import Typography from './Typography'
 import Burger from './Burger'
 import Spacer from './Spacer'
+import Menu from '../organisms/Menu'
 
 
-export type Props = ButtonProps
+export interface Props extends ButtonProps {
+  onOpenChange(open: boolean): void, 
+}
 
-export default function MenuButton (props: Props) {
+export default function MenuButton ({onOpenChange}: Props) {
+  const [open, setOpen] = useState<boolean>(false)
+
+  useEffect(() => {
+    const html = document.documentElement
+
+    if (open) {
+      html.classList.add('no-scroll')
+    } else {
+      html.classList.remove('no-scroll')
+    }
+  })
+
   return (
-    <Button {...props} variant='no-style'>
-      <Wrapper>
-        <Burger />
-        <Spacer size={10} axis='horizontal'></Spacer>
-        <Typography variant='button'>Menu</Typography>
-      </Wrapper>
-    </Button>
+    <>
+      <Button variant='no-style' onClick={() => {setOpen(!open); onOpenChange(!open)}}>
+        <Wrapper>
+          <Burger />
+          <Spacer size={10} axis='horizontal' />
+          <Typography variant='button'>Menu</Typography>
+        </Wrapper>
+      </Button>
+      <Menu open={open}/>
+    </>
   )
 }
 
