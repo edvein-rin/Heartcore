5096c408c56d2b41a2688cae6c8a93410f28a392
edvein-rin <edveinrin@gmail.com>
2021-07-30 11:03:01 +0300

    Expanding Button to be able to customize colors

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,4 +1,5 @@
 import styled, { css } from 'styled-components/macro'
+import { Color } from '../globals/Colors'
 import Typography from './Typography'
 
 
@@ -6,14 +7,25 @@ export interface Props {
   display?: 'block' | 'inline' | 'flex',
   variant?: 'text' | 'no-style',
   textCase?: 'lowercase' | 'uppercase' | 'capitalize' | 'none',
+  textColor?: Color,
+  textHoverColor?: Color,
   bold?: boolean,
   italic?: boolean,
   children?: React.ReactNode,
 }
 
-export default function Button ({children, display = 'block', variant = 'text', textCase = 'uppercase', bold = true, italic = false}: Props) {
+export default function Button ({
+                              children,
+                              display,
+                              variant = 'text',
+                              textCase = 'uppercase',
+                              textColor,
+                              textHoverColor = 'orange',
+                              bold = true,
+                              italic = false,
+                                }: Props) {
   return (
-    <Wrapper display={display} variant={variant} textCase={textCase} bold={bold} italic={italic}>
+    <Wrapper display={display} variant={variant} textCase={textCase} textColor={textColor} textHoverColor={textHoverColor} bold={bold} italic={italic}>
       {variant === 'text' ? <Typography variant='button'>{children}</Typography> : children}
     </Wrapper>
   )
@@ -21,10 +33,16 @@ export default function Button ({children, display = 'block', variant = 'text', 
 
 export const Wrapper = styled.div<Props>`
   display: ${({display}) => display};
+  
   font-weight: ${({bold}) => bold ? 'bold' : 'inherit'};
   font-style: ${({italic}) => italic ? 'italic' : 'inherit'};
+  color: ${({textColor}) => 'var(--' + textColor + ')'};
 
   cursor: pointer;
+
+  &:hover {
+    color: ${({textHoverColor}) => 'var(--' + textHoverColor + ')'};
+  }
   
   ${({variant, textCase}) => {
     switch (variant) {
