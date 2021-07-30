01e0e52e22714dbeaa1ee542b29955d839ef0e2c
edvein-rin <edveinrin@gmail.com>
2021-07-30 15:48:24 +0300

    Added width and height

--- src/components/atoms/ImageLink.tsx
+++ src/components/atoms/ImageLink.tsx
@@ -1,14 +1,26 @@
+import styled from "styled-components";
 import Link, { Props as LinkProps } from "./Link";
 
 export interface Props extends LinkProps {
   alt: string,
   src?: string,
+  height?: string,
+  width?: string,
 }
 
 export default function ImageLink (props: Props) {
   return (
-    <Link href={props.href}>
-      <img alt={props.alt} src={props.src}></img>
-    </Link>
+    <Wrapper href={props.href} height={props.height} width={props.width}>
+      <img alt={props.alt} src={props.src} />
+    </Wrapper>
   )
 }
+
+export const Wrapper = styled(Link)`
+  width: ${({width}) => width};
+  height: ${({height}) => height};
+
+  & > img {
+    height: 100%;
+  }
+`
